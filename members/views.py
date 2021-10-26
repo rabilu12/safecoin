c71a050b7fd74b44411d2e665450925b657fc97e
rabilu12 <87603563+rabilu12@users.noreply.github.com>
2021-10-26 21:32:23 +0100

    Create views.py

--- members/views.py
+++ members/views.py
@@ -251,9 +251,6 @@ def give(request):
     
 @(login_required(login_url='user:log_in'))
 def feeverify(request):
-    vid = request.user.id
-    pbalance=vpp_balance.objects.get(vpp_id=vid)
-    balance = int(pbalance.unit)
     form1 = vpp_balanceForm()
     form = PaidForm()
     context = {'form':form, 'balance':balance}
