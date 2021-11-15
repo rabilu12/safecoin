4aafa39b7697de7b8fc4088b5a10fa61714315d4
rabilu12 <87603563+rabilu12@users.noreply.github.com>
2021-11-15 14:50:12 +0100

    Update views.py

--- members/views.py
+++ members/views.py
@@ -14,7 +14,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.forms.utils import ErrorList
 from django.http import HttpResponse
-from .forms import AgentForm, LoginForm, PaidForm, SignUpForm, vpp_balanceForm, vppsubForm, iduploadForm, orphanageForm, transactionForm
+from .forms import AgentForm, LoginForm, PaidForm, SignUpForm, vpp_balanceForm, vppsubForm, iduploadForm, orphanageForm, transactionForm, ProfileForm
 
 
 @login_required(login_url="log_in")
