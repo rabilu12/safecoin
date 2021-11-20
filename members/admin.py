8f00c640308320a06b6f7f3e76a02b3d92f21b2a
rabilu12 <87603563+rabilu12@users.noreply.github.com>
2021-11-20 14:38:16 +0100

    Update admin.py

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from members.models import Agent, Agent_verified, Profile, Paid, Vpp, orphanage, vpp_balance, vppsub, Transaction, Circle
+from members.models import Agent, Agent_verified, Profile, Paid, Vpp, orphanage, vpp_balance, vppsub, Transaction
 
 admin.site.register(Profile)
 admin.site.register(Agent)
@@ -11,4 +11,3 @@ admin.site.register(vppsub)
 admin.site.register(Agent_verified)
 admin.site.register(orphanage)
 admin.site.register(Transaction)
-admin.site.register(Circle)
