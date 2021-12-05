abc9268a5b9e9d88e230bdade559bbe2ade4021e
rabilu12 <87603563+rabilu12@users.noreply.github.com>
2021-12-05 21:14:35 +0100

    Update conf.py

--- safecoin/cdn/conf.py
+++ safecoin/cdn/conf.py
@@ -13,7 +13,7 @@ STATICFILES_STORAGE = "StaticRootS3Boto3Storage"
 DEFAULT_FILE_STORAGE = "safecoin.cdn.backends.MediaRootS3Boto3Storage"
 
 STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, 'static')  
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # dev example
+STATIC_ROOT = 'static/'
 
 MEDIA_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, 'media') 
 MEDIA_ROOT = 'media/'
