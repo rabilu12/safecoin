38d63bdaeaacc72049f99b2dd693a870535ee1fb
rabilu12 <87603563+rabilu12@users.noreply.github.com>
2021-12-05 15:13:16 +0100

    Update conf.py

--- safecoin/cdn/conf.py
+++ safecoin/cdn/conf.py
@@ -1,4 +1,5 @@
 import os
+from .cdn.conf import *  # noqa
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "charityland"
