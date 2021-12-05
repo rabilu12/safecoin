4eaa6de9bbec4bfe9042c4bc1da0c57f0b2c6348
rabilu12 <87603563+rabilu12@users.noreply.github.com>
2021-12-05 17:46:37 +0100

    Update settings.py

--- safecoin/settings.py
+++ safecoin/settings.py
@@ -1,3 +1,4 @@
+from .cdn.conf import *  # noqa
 import os
 from pathlib import Path
 import sys
@@ -151,12 +152,12 @@ USE_TZ = True
 #Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = (
+   # os.path.join(BASE_DIR, 'static'),
+#)
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
 
 # MEDIA INFORMATION:
 #MEDIA_ROOT = MEDIA_DIR
